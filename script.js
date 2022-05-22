71d4ac82be429174e90c7a9e2a86b38b4ffa0b67
mriazh <192410102059@mail.unej.ac.id>
2022-05-22 12:36:13 +0700

    headache like button

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+// like button
 $(document).ready(function(){
     $(".like").click(function(){
         $(this).toggleClass("heart");
     });
+    // $(".get-course-text").click(function(){
+    //     $(".like").toggleClass("heart");
+    // });
 });
