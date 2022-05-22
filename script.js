dadb2108726566a4711537c3db973dc63a8609b1
mriazh <192410102059@mail.unej.ac.id>
2022-05-22 20:36:01 +0700

    filter-items

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ $(document).ready(function(){
     //     $(".like").toggleClass("heart");
     // });
 });
+function fav(id){
+    // $(`#${id}`).toggleClass("heart");
+    $("#" + id).toggleClass("heart");
+}
+
+// filter-item
+$("#category-filter").change(function(){
+    let filter = $(this).val();
+    filterList(filter);
+});
+function filterList(value) {
+    let list = $(".box-container");
+    $(list).hide();
+    if(value == "all"){
+        $(".filter-section").find('.box-container').each(function(i){
+            $(this).show();
+        });
+    } else{
+        $(".filter-section").find("." + value).each(function(i){
+            $(this).show();
+        });
+    }
+}
+
+// expand-and-collapse
